c0afef68b34129645fc2f200e3ec3903c866d5f5
IrfanaAslam <78956736+IrfanaAslam@users.noreply.github.com>
2025-05-07 22:12:39 +0500

    Add files via upload

---
+++ basic_cpp_profile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name="My Name is Irfana Aslam!";
+    int age=26;
+    float Height=5.4;
+
+    cout << "Name: "<<name << endl;
+    cout << "My age is: "<<age << endl;
+    cout << "My Height is: " << Height<< endl;
+
+
+    system("pause");
+    return 0;
+}
